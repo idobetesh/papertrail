592b07519df4439e4542820a48a7b94045167976
betei <ido.betesh@autodesk.com>
2026-01-10 11:51:01 +0200

    chore: project setup and tooling
    - ESLint, Prettier, Husky pre-commit hooks
    - Node.js 24 (.nvmrc)
    - Root package.json with workspaces

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/*.js', '!eslint.config.js'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      // TypeScript specific rules
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+
+      // General rules
+      'no-console': 'off', // We use console for logging
+      'prefer-const': 'error',
+      'no-var': 'error',
+      eqeqeq: ['error', 'always'],
+      curly: ['error', 'all'],
+    },
+  },
+];
